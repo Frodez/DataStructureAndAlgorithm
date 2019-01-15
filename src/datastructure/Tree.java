371eec4b4fa00dbae99219461da4052f470fbcb2
Frodez <463082995@qq.com>
2019-01-15 19:50:47 +0800

    完成普通树的非递归先序遍历和后序遍历

--- src/datastructure/Tree.java
+++ src/datastructure/Tree.java
@@ -244,9 +244,11 @@ public class Tree<T extends Comparable<T>> {
 		Node<T> node = root;
 		Stack<Node<T>> stack = new Stack<>();
 		while (true) {
-			//先对自己进行操作,并将自己压入栈中.
+			//先对自己进行操作,并将自己压入栈中.(此为父->左->右中的父)
 			//如果有子节点,则指向最左边的子节点.
-			//如果没有子节点,则将栈顶元素退出,直到退出的栈顶元素拥有下一个兄弟节点为止.此时指向下一个兄弟节点.
+			//如果没有子节点,则将栈顶元素退出.(没有子节点时,由于一直从左边起遍历,因此自己一定是父节点的最左子节点)
+			//直到退出的栈顶元素拥有下一个兄弟节点为止.
+			//此时指向下一个兄弟节点.(下一个兄弟节点即意味着从左往右)
 			//如果退出的栈顶元素为根节点,则终止循环.
 			consumer.accept(node.data, node.depth);
 			stack.push(node);
@@ -259,6 +261,7 @@ public class Tree<T extends Comparable<T>> {
 						return;
 					}
 					if (temp.next != null) {
+						node = temp.next;
 						break;
 					}
 				}
@@ -300,8 +303,10 @@ public class Tree<T extends Comparable<T>> {
 		while (true) {
 			//如果当前节点存在子节点,则将当前节点压入栈中,并指向第一个子节点.
 			//如果当前节点不存在子节点,则操作本元素,且判断是否存在下一个兄弟节点.
-			//如果存在下一个兄弟节点,则指向下一个兄弟节点.
-			//如果不存在下一个兄弟节点,则将栈顶元素弹出,判断其是否存在下一个兄弟节点,直到存在下一个兄弟节点为止.
+			//(没有子节点时,由于一直从左边起遍历,因此自己一定是父节点的最左子节点)
+			//如果存在下一个兄弟节点,则指向下一个兄弟节点.(从左到右的顺序)
+			//如果不存在下一个兄弟节点,则将栈顶元素弹出,并操作.(最后操作父节点)
+			//然后判断其是否存在下一个兄弟节点,直到存在下一个兄弟节点为止.
 			//此时指向下一个兄弟节点.
 			//如果弹出元素为根节点,则退出循环.
 			if (node.firstChild != null) {
@@ -312,6 +317,7 @@ public class Tree<T extends Comparable<T>> {
 				if (node.next == null) {
 					while (true) {
 						Node<T> temp = stack.pop();
+						consumer.accept(temp.data, temp.depth);
 						if (temp == root) {
 							return;
 						}
